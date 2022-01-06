133d85853237dfa080d71687eda83c7c765a4e60
Christopher Schneider <schneiderchristopher16@gmail.com>
2022-01-06 16:05:16 -0300

    Initial Styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { useState } from 'react'
 import Select from 'react-select'
 
 const options = [
@@ -11,10 +9,18 @@ const options = [
 ]
 
 const Home: NextPage = () => {
+  const [championOne, setchampionOne] = useState<string | undefined>('Nenhum')
+  const [championTwo, setchampionTwo2] = useState<string | undefined>('Nenhum')
   return (
-    <div className='bg-background flex justify-center h-screen items-center'>
-      <h1 className='text-white text-4xl'>LeagueMatches.gg</h1>
-      <Select options={options}/>
+    <div className='bg-[url("../public/qiyana.jpg")] bg-cover flex justify-center h-screen items-center flex-col'>
+      <div className='bg-black/40'>
+      <h1 className='text-pink-400 outline outline-pink-500 px-5 py-4 text-4xl rounded-sm drop-shadow-xl'>LeagueMatches.gg</h1>
+      </div>
+      <div className='flex m-4 mt-5 w-[100%] justify-center items-center'>
+      <Select options={options} onChange={(value) => setchampionOne(value?.value)} className='m-2 w-44 mx-8 border-purple-200'/>
+      <button className='bg-gradient-to-r from-soft to-primary p-2 rounded px-8 drop-shadow-xl ring-2 ring-background/50 hover:drop-shadow-2x hover:ring-white/20'>VS</button>
+      <Select options={options} onChange={(value) => setchampionTwo2(value?.value)} className='m-2 w-44 mx-8'/>
+      </div>
     </div>
   )
 }
