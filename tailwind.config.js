d20a1a008a991870419e8a6a09b6fa9ddcc0504b
Christopher Schneider <schneiderchristopher16@gmail.com>
2022-01-06 14:05:20 -0300

    Tailwind Configuration

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: '#242582',
+        primary: '#F64C72',
+        soft: '#553D67',
+        lighter: '#99738E',
+        backgroundBright: '#2F2FA2'
+      }
+    },
+  },
+  plugins: [],
+}
