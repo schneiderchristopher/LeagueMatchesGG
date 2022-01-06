1b23f9331a9c7cb3004ec293c5f5e7b19ce642e3
Christopher Schneider <schneiderchristopher16@gmail.com>
2022-01-06 15:13:06 -0300

    Added react-select module

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,19 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Select from 'react-select'
+
+const options = [
+  { value: 'katarina', label: 'Katarina'},
+  { value: 'lux', label: 'Lux'},
+  { value: 'veigar', label: 'Veigar'}
+]
 
 const Home: NextPage = () => {
   return (
     <div className='bg-background flex justify-center h-screen items-center'>
       <h1 className='text-white text-4xl'>LeagueMatches.gg</h1>
+      <Select options={options}/>
     </div>
   )
 }
